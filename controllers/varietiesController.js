462c9c067676965e35c0854d40af7102ee321a58
pb-bennett <pb.bennett.uk@gmail.com>
2023-01-29 11:05:06 +0100

    written catchAsync utility function to catch errors in async function

--- controllers/varietiesController.js
+++ controllers/varietiesController.js
@@ -3,144 +3,118 @@ const Joi = require('joi');
 const sequelize = require('../db');
 const initModels = require('../models/init-models');
 const APIQueries = require('../utils/apiQueries');
+const searchBuilder = require('sequelize-search-builder');
 
 const models = initModels(sequelize);
-
-const searchBuilder = require('sequelize-search-builder');
+const catchAsync = require('../utils/catchAsync');
 
 // console.log(models);
 
-exports.searchAllVarieties = async function (req, res, next) {
-  try {
-    // console.log(viableComlumns);
-    const search = new searchBuilder(models.varieties, req.query),
-      whereQuery = search.getWhereQuery(),
-      orderQuery = search.getOrderQuery(),
-      limitQuery = search.getLimitQuery(),
-      offsetQuery = search.getOffsetQuery();
-    // const apiQueries = new APIQueries(models.varieties, req.query);
-    // const query = apiQueries.filter();
+exports.searchAllVarieties = catchAsync(async function (req, res, next) {
+  // console.log(viableComlumns);
+  const search = new searchBuilder(models.varieties, req.query),
+    whereQuery = search.getWhereQuery(),
+    orderQuery = search.getOrderQuery(),
+    limitQuery = search.getLimitQuery(),
+    offsetQuery = search.getOffsetQuery();
 
-    const results = await models.varieties.findAll({
-      // include: [{ all: true, nested: false, duplicating: false }],
-      include: [{ model: models.varietiesPhotos, as: 'varietiesPhotos' }],
-      where: whereQuery,
-      order: orderQuery,
-      limit: limitQuery,
-      offset: offsetQuery,
-      // logging:
+  const results = await models.varieties.findAll({
+    // include: [{ all: true, nested: false, duplicating: false }],
+    include: [{ model: models.varietiesPhotos, as: 'varietiesPhotos' }],
+    where: whereQuery,
+    order: orderQuery,
+    limit: limitQuery,
+    offset: offsetQuery,
+    // logging:
+  });
+
+  return res.status(200).json({
+    status: 'success',
+    length: results.length,
+    results: results,
+  });
+});
+
+exports.getAllVarieties = catchAsync(async function (req, res, next) {
+  const results = await models.varieties.findAll({
+    // include: [{ all: true, nested: true, duplicating: false }],
+  });
+
+  return res.status(200).json({
+    status: 'success',
+    length: results.length,
+    results: results,
+  });
+});
+
+exports.newVariety = catchAsync(async function (req, res, next) {
+  const { varietyName, plantType } = req.body;
+
+  const result = await models.varieties.create({ varietyName, plantType });
+  console.log(result);
+
+  return res.status(200).json({
+    status: 'success',
+    results: result.dataValues,
+  });
+});
+
+exports.getVariety = catchAsync(async function (req, res, next) {
+  const id = req.params.id;
+  const result = await models.varieties.findByPk(id);
+  if (!result)
+    return res.status(404).json({
+      status: 'error',
+      result: `ID ${id} not found in database.`,
     });
 
-    return res.status(200).json({
-      status: 'success',
-      length: results.length,
-      results: results,
+  return res.status(200).json({
+    status: 'success',
+    results: result,
+  });
+});
+
+exports.updateVariety = catchAsync(async function (req, res, next) {
+  const id = req.params.id;
+  const variety = await models.varieties.findByPk(id);
+  if (!variety)
+    return res.status(404).json({
+      status: 'failure',
+      results: `ID ${id} not found in database.`,
     });
-  } catch (error) {
-    console.error(error);
-  }
-};
+  await models.varieties.update(req.body, {
+    where: {
+      id,
+    },
+  });
+  const result = await models.varieties.findByPk(id);
 
-exports.getAllVarieties = async function (req, res, next) {
-  try {
-    const results = await models.varieties.findAll({
-      // include: [{ all: true, nested: true, duplicating: false }],
+  console.log('😆😆😆', result);
+
+  return res.status(200).json({
+    status: 'success',
+    results: result,
+  });
+});
+
+exports.deleteVariety = catchAsync(async function (req, res, next) {
+  const id = req.params.id;
+  const variety = await models.varieties.findByPk(id);
+  if (!variety)
+    return res.status(404).json({
+      status: 'failure',
+      results: `ID ${id} not found in database.`,
     });
-
-    return res.status(200).json({
-      status: 'success',
-      length: results.length,
-      results: results,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-exports.newVariety = async function (req, res, next) {
-  try {
-    const { varietyName, plantType } = req.body;
-
-    const result = await models.varieties.create({ varietyName, plantType });
-    console.log(result);
-
-    return res.status(200).json({
-      status: 'success',
-      results: result.dataValues,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-exports.getVariety = async function (req, res, next) {
-  try {
-    const id = req.params.id;
-    const result = await models.varieties.findByPk(id);
-    if (!result)
-      return res.status(404).json({
-        status: 'error',
-        result: `ID ${id} not found in database.`,
-      });
-
-    return res.status(200).json({
-      status: 'success',
-      results: result,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-exports.updateVariety = async function (req, res, next) {
-  try {
-    const id = req.params.id;
-    const variety = await models.varieties.findByPk(id);
-    if (!variety)
-      return res.status(404).json({
-        status: 'failure',
-        results: `ID ${id} not found in database.`,
-      });
-    await models.varieties.update(req.body, {
-      where: {
-        id,
-      },
-    });
-    const result = await models.varieties.findByPk(id);
-
-    console.log('😆😆😆', result);
-
-    return res.status(200).json({
-      status: 'success',
-      results: result,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-exports.deleteVariety = async function (req, res, next) {
-  try {
-    const id = req.params.id;
-    const variety = await models.varieties.findByPk(id);
-    if (!variety)
-      return res.status(404).json({
-        status: 'failure',
-        results: `ID ${id} not found in database.`,
-      });
-    await models.varieties.destroy({
-      where: {
-        id,
-      },
-    });
-    return res.status(200).json({
-      status: 'success',
-      results: 'Deleted',
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
+  await models.varieties.destroy({
+    where: {
+      id,
+    },
+  });
+  return res.status(200).json({
+    status: 'success',
+    results: 'Deleted',
+  });
+});
 
 // const test = async function () {
 //   try {
